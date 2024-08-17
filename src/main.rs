9957c6658a4beb2cbff8024c62cd5c6902638b85
Temesgen Ataro <atarotemesgen@gmail.com>
2024-08-18 01:30:33 +0400

    Refactor src/main.rs
    We no longer need the extra hand holding comments

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,25 @@
-// ===========
-// Req 1, 2, 3
-// ===========
 #![no_std]
 #![no_main]
 
-// Ensure we halt the program on panic (if we don't mention this crate it won't
-// be linked)
 use panic_halt as _;
 
-// Alias for our HAL crate
 use rp2040_hal as hal;
-
-// A shorter alias for the Peripheral Access Crate, which provides low-level
-// register access
 use hal::pac;
-
-// ===========
-// Req 4
-// ===========
 use embedded_hal::delay::DelayNs;
-use embedded_hal::digital::OutputPin; // or digital::{InputPin, OutputPin} for both
+use embedded_hal::digital::OutputPin;
 
-// ===========
-// Req 5
-// ===========
 use rp2040_boot2;
 #[link_section = ".boot2"]
 #[used]
 pub static BOOT_LOADER: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
-// ===========
-// Req 6
-// ===========
 const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+const UNIT_DELAY: u32 =  31_770_000;      // in nanoseconds
+                                          // every 525 unit delays, vsync also updates
+const HSYNC_TO_VSYNC_RATIO: u32 =  50;    // in nanoseconds
 
-
-const UNIT_DELAY: u32 =  31_770_000; // in nanoseconds
-                              // every 525 unit delays, vsync also updates
-const HSYNC_TO_VSYNC_RATIO: u32 =  50;  // in nanoseconds
-// ===========
-// Req 7
-// ===========
 #[rp2040_hal::entry]
 fn main() -> ! {
-    // Main doesn't return anything.
-    // ===========
-    // Req 8
-    // ===========
     let mut pac = pac::Peripherals::take().unwrap(); // Handle errors if take() fails
     let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
 
@@ -60,21 +33,10 @@ fn main() -> ! {
         &mut watchdog,
     )
     .unwrap();
-    // ===========
-    // Req 9
-    // ===========
-    // Requires pac, clocks and watchdogs before init
     let mut timer = rp2040_hal::Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
 
-    // ===========
-    // Req 10
-    // ===========
-    // Requires pac singleton before init
     let sio = hal::Sio::new(pac.SIO);
 
-    // ===========
-    // Req 11
-    // ===========
     let pins = hal::gpio::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
